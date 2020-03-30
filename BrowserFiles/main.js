98793a360c0ebfabdc535909d7429bc7d19b888f
unknown <siddhartha.pawar17@gmail.com>
2020-03-30 19:58:06 +0530

    Initial commit

---
+++ BrowserFiles/main.js
@@ -0,0 +1,13 @@
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+let width = canvas.width = window.innerWidth;
+let height = canvas.height = window.innerHeight;
+
+// context.fillRect(0, 0, width, height);
+
+for (let i = 1; i <= 100; i++) {
+    context.beginPath();
+    context.moveTo(Math.random() * width, Math.random() * height);
+    context.lineTo(Math.random() * width, Math.random() * height);
+    context.stroke();
+}
